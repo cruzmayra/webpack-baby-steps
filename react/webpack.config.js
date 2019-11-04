eebdc369a5e9508fce9878fa93f55f0fa3a8ec16
cruzmayra <velazquez.leonora@gmail.com>
2019-11-04 00:03:30 -0600

    Soporte a JSX

---
+++ react/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  entry:  {
+    home: path.resolve(__dirname, 'src/js/index.js')
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js' /* template de webpack */
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          /* se sustituye por style-loader */
+          // {
+          //   loader: MiniCSSExtractPlugin.loader
+          // },
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Soporte a JSX',
+      template: path.resolve(__dirname, 'index.html')
+    })
+    /* se elimina este plugin para mejorar la eficiencia, es más fácil inyectar el css que extraerlo */
+    // new MiniCSSExtractPlugin({
+    //   filename: 'css/[name].css'
+    // })
+  ]
+}
